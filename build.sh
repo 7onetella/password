efd2bdaf1cf53052402cc1549cbd7380812a4257
7onetella <7onetella@gmail.com>
2019-10-25 17:15:21 -0400

    fixing

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ set -x
 
 cd ui
 
-rm -rf dist/* && ember build --environment=${1} && cp -r dist/* ../api/ui/
+npm install
+
+rm -rf dist/*
+ember build --environment=${1} 
+cp -r dist/* ../api/ui/
 
 cd ..
 
