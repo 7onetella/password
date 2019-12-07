b6fb28703b96b3dc16324f485795dd9df6bd6cd7
7onetella <7onetella@gmail.com>
2019-12-07 09:35:50 -0500

    wip

--- api/bin/test.sh
+++ api/bin/test.sh
@@ -17,11 +17,14 @@ SERVER_HOST=$(dig @127.0.0.1 -p 8600 password-dev-app.service.dc1.consul. SRV | 
 SERVER_PORT=$(dig @127.0.0.1 -p 8600 password-dev-app.service.dc1.consul. SRV | grep "IN SRV" | awk '{ print $7 }')
 
 if [ "${STAGE}" = "localhost" ]; then
+   SERVER_HOST="localhost"
    SERVER_PORT=4242
 fi
 
 SERVER_ADDR="${SERVER_HOST}:${SERVER_PORT}"
 export SERVER_ADDR
 export INSECURE=true
+
+dig @127.0.0.1 -p 8600 password-dev-app.service.dc1.consul. SRV
 
 go test -v ./...
