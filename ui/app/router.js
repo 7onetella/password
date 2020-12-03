6edfbd401e2a3e53ba15bab5d796230616dcbd51
Seven Tella <7onetella@gmail.com>
2020-12-03 02:29:53 -0500

    adding fav icon and logo

--- ui/app/router.js
+++ ui/app/router.js
@@ -10,7 +10,6 @@ Router.map(function() {
   this.route('passwords');
   this.route('passwords/new');
   this.route('passwords/edit', { path: '/passwords/edit/:id'});
-  this.route('about');
   this.route('signin');
 });
 
