065b4579b1685008d6e07fa3793ce11f624331fa
Seven Tella <7onetella@gmail.com>
2020-12-20 23:41:24 -0500

    adding deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=$(cat version | awk '{$1=$1;print}' | tail +1)
+
+cd api
+
+image=docker-registry.7onetella.net:5000/7onetella/password:"${version}"
+
+docker build -t "${image}" .
+
+docker push "${image}"
+
+vag docker deploy password-dev:"${version}"
