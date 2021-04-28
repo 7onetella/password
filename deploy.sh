edf233a8cfcb293d90aa257f9378560e893203c0
Seven Tella <7onetella@gmail.com>
2021-04-28 00:45:39 -0400

    updating build

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ version=$(cat version | awk '{$1=$1;print}' | tail +1)
 
 cd api
 
-image=docker-registry.7onetella.net:5000/7onetella/password:"${version}"
+image=docker-registry.7onetella.net/7onetella/password:"${version}"
 
 docker build -t "${image}" .
 
